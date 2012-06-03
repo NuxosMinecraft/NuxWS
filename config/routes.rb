492bfa22ff4765aff340948953bc4195c4f36fd2
Marc Lagrange <rhaamo@gruik.at>
2012-06-04 00:01:46 +0200

    Add Places views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 NuxWS::Application.routes.draw do
   resources :users
   resources :user_sessions
+  resources :places
   
   match "/login", :to => "user_sessions#new"
   match "/logout", :to => "user_sessions#destroy"
