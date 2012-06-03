527f903655dc90059ab8808a0759aa90f56b396c
Marc Lagrange <rhaamo@gruik.at>
2012-06-03 16:55:09 +0200

    First import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NuxWS::Application.config.secret_token = '7179e2cd10dffc353c80f1da6faea5173cf3b6ec0517d54492beb75a1513a81dcfd0d96c792b78a8964f8b4459029b8cbe5353922de2f5c2b0b551d2829b73fa'
