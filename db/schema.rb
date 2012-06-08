eb977d9af78a661635c032bfc200ba6c88c6da03
Marc Lagrange <rhaamo@gruik.at>
2012-06-08 09:08:34 +0200

    Disable assets debug logging, remove rails_config, add ledermann-rails-settings, settings panel todo.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120608062200) do
+ActiveRecord::Schema.define(:version => 20120608070302) do
 
   create_table "forums", :force => true do |t|
     t.string   "title"
@@ -75,6 +75,17 @@ ActiveRecord::Schema.define(:version => 20120608062200) do
     t.datetime "updated_at", :null => false
     t.integer  "rid"
   end
+
+  create_table "settings", :force => true do |t|
+    t.string   "var",                       :null => false
+    t.text     "value"
+    t.integer  "target_id"
+    t.string   "target_type", :limit => 30
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "settings", ["target_type", "target_id", "var"], :name => "index_settings_on_target_type_and_target_id_and_var", :unique => true
 
   create_table "topics", :force => true do |t|
     t.string   "title",                         :null => false
