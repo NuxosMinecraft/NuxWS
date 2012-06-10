f3c0e8bbad837057a24c2f6a660ea783a767b828
Marc Lagrange <rhaamo@gruik.at>
2012-06-10 22:21:05 +0200

    add markdown_toc helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 module ApplicationHelper
   def markdown(text)
-    md = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
+    md = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(:filter_html => true, :with_toc_data => true), :autolink => true, :space_after_headers => true)
     md.render(text).html_safe
   end
+  
+  def markdown_toc(text)
+    md = Redcarpet::Markdown.new(Redcarpet::Render::HTML_TOC)
+    md.render(text).html_safe
+  end
+  
 end
