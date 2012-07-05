77a2c6454e673b06f1e619b88816184b1d8c7813
Marc Lagrange <rhaamo@gruik.at>
2012-07-05 22:27:49 +0200

    Rewrote rights dependencies

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,41 +1,52 @@
 class Ability
   include CanCan::Ability
 
+  def default(user)
+    puts "Rights: default"
+    can :read, :all
+    can :manage, User, :id => user.id
+    cannot :destroy, User, :id => user.id
+  end
+  
+  def guest(user)
+    puts "Rights: guest"
+    can :create, Topic, Message
+    can :edit, [Topic, Message], :user_id => user.id
+  end
+  
+  def padawan(user)
+    puts "Rights: padawan"
+    cannot [:create, :edit, :destroy], Message, :topic => {:locked => true}
+    cannot [:edit, :destroy], Topic, :locked => true
+    cannot [:edit, :destroy], Message, :deleted => true 
+  end
+  
+  def player(user)
+    puts "Rights: player"
+    can :manage, [Gallery, Image] # no specific user association
+    can :manage, [Place, Topic, Message], :user_id => user.id
+    cannot :destroy, [Place, Topic, Message], :user_id => user.id
+    can :manage, [Doc], :modos_only => false
+  end
+  
+  def moderator_admin(user)
+    puts "Rights: moderator or admin"
+    can :manage, [Place, Forum, Topic, Message, Gallery, Image, Doc, ForumCategory, User]
+    can :read, Log
+  end
+
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     # roles :
     # 1:guest, 4:padawan, 8:player, 12:moderator, 16:admin
     if !user.role or user.new_record?
-      # Guest
-      can :read, :all
-      can :create, User
+      default(user)
     else
-      can :read, :all
-      can :manage, User, :id => user.id
-      cannot :destroy, User, :id => user.id
-      can :create, Topic
-      
-      if user.role.rid == 4 or user.role.rid == 8
-        # padawan or player
-        can :manage, [Gallery, Image] # no specific user association
-        can :manage, [Place, Topic, Message], :user_id => user.id
-        cannot :destroy, [Place, Topic, Message], :user_id => user.id
-        can :manage, [Doc], :modos_only => false
-        cannot [:create, :edit, :destroy], Message, :topic => {:locked => true}
-        cannot [:edit, :destroy], Topic, :locked => true
-        cannot [:edit, :destroy], Message, :deleted => true
-        
-      end
-      if user.role.rid == 12
-        # moderator
-        can :manage, [Place, Forum, Topic, Message, Gallery, Image, Doc, ForumCategory, User]
-        can :read, Log
-      end
-      if user.role.rid == 16
-        # Admin
-        can :manage, [Place, Forum, Topic, Message, Gallery, Image, Doc, ForumCategory, User]
-        can :read, Log
-      end
+      default(user)
+      guest(user) if user.role.rid >= 1
+      padawan(user) if user.role.rid >= 4
+      player(user) if user.role.rid >= 8
+      moderator_admin(user) if user.role.rid >= 12 # will match 16(admin) too
     end
   end
 end
