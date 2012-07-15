5fe15b02e2caec752619456fc7150f5fd86baa83
Marc Lagrange <rhaamo@gruik.at>
2012-07-15 20:14:21 +0200

    Restrict changes of modos_only

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -2,11 +2,28 @@ class Doc < ActiveRecord::Base
   attr_accessible :title, :content, :modos_only, :description
 
   default_scope :order => 'created_at DESC'
-  
+  before_update :restrict_update_attrs
+
   extend FriendlyId
   friendly_id :title, use: :slugged
-    
+
   paginates_per Settings.pagination_docs
-  
+
   validates_presence_of :title, :description, :content
+
+  def restrict_update_attrs
+    restrict = 0
+    if User.current
+      if !User.current.at_least_modo?
+        restrict = 1
+      end
+    else
+      restrict = 1
+    end
+
+    if !restrict
+      self.modos_only = self.modos_only_was if self.modos_only_changed?
+    end
+  end
+
 end
