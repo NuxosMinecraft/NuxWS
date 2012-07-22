8489432ff042b607e99e876e6632e858d0c121fc
Marc Lagrange <rhaamo@gruik.at>
2012-07-22 20:22:00 +0200

    Add support for remote picture location

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,16 +2,16 @@ class Gallery < ActiveRecord::Base
   attr_accessible :title, :description, :place_id, :images_attributes
 
   default_scope :order => 'created_at DESC'
-  
+
   extend FriendlyId
   friendly_id :title, use: :slugged
-    
+
   has_many :images, :dependent => :delete_all
   has_one :place
-  
+
   accepts_nested_attributes_for :images, :allow_destroy => true
-  
+
   paginates_per Settings.pagination_galleries.to_i
-  
+
   validates_presence_of :title
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,16 +1,16 @@
 class Image < ActiveRecord::Base
-  attr_accessible :title, :description, :picture, :picture_cache
+  attr_accessible :title, :description, :picture, :picture_cache, :remote_picture_url
   mount_uploader :picture, PictureUploader
 
   default_scope :order => 'created_at ASC'
-  
+
   extend FriendlyId
   friendly_id :title, use: :slugged
-    
+
   belongs_to :gallery
-  
+
   validates_associated :gallery
   validates_presence_of :title, :picture
-  
+
   paginates_per Settings.pagination_images.to_i
 end
