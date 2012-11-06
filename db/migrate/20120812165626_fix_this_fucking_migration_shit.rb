e0133f52bbcb2b5fcbffaf7fab9d06c96b6166ba
Marc Lagrange <rhaamo@sigpipe.me>
2012-11-06 11:18:24 +0100

    Migrations...

--- db/migrate/20120812165626_fix_this_fucking_migration_shit.rb
+++
@@ -1,6 +0,0 @@
-class FixThisFuckingMigrationShit < ActiveRecord::Migration
-  def up
-    add_column :topic_notifications, :user_id, :integer
-    add_column :topic_notifications, :topic_id, :integer
-  end
-end
