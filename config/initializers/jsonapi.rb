78fb823b90ff2aed6df455dcbdb9afebe4f35277
Marc Lagrange <rhaamo@sigpipe.me>
2012-12-23 10:08:35 +0100

    Skip JSONAPI when in Rake

--- config/initializers/jsonapi.rb
+++ config/initializers/jsonapi.rb
@@ -1,9 +1,12 @@
 # Initialize JSONAPI
 
-jsonapi_config = YAML.load_file("#{Rails.root}/config/jsonapi.yml")
-begin
-  JsonApi = JSONAPI::JSONAPI.new(jsonapi_config)
-rescue Faraday::Error::ConnectionFailed
-  JsonApi = nil
+if !defined?(::Rake)
+  puts "JSONAPI: Connecting"
+  jsonapi_config = YAML.load_file("#{Rails.root}/config/jsonapi.yml")
+  begin
+    JsonApi = JSONAPI::JSONAPI.new(jsonapi_config)
+  rescue Faraday::Error::ConnectionFailed
+    JsonApi = nil
+  end
+  puts "JSONAPI: Connected" if JsonApi
 end
-puts "JSONAPI: Connected" if JsonApi
