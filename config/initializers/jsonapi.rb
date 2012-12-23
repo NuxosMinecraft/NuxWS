09a366cb7ffea57682f72aec6133146bf63fb0d9
Marc Lagrange <rhaamo@sigpipe.me>
2012-12-23 10:45:16 +0100

    Disabled JsonApi, update fucked the API

--- config/initializers/jsonapi.rb
+++ config/initializers/jsonapi.rb
@@ -1,12 +1,14 @@
 # Initialize JSONAPI
 
-if !defined?(::Rake)
-  puts "JSONAPI: Connecting"
-  jsonapi_config = YAML.load_file("#{Rails.root}/config/jsonapi.yml")
-  begin
-    JsonApi = JSONAPI::JSONAPI.new(jsonapi_config)
-  rescue Faraday::Error::ConnectionFailed
-    JsonApi = nil
-  end
-  puts "JSONAPI: Connected" if JsonApi
-end
+#if !defined?(::Rake)
+#  puts "JSONAPI: Connecting"
+#  jsonapi_config = YAML.load_file("#{Rails.root}/config/jsonapi.yml")
+#  begin
+#    JsonApi = JSONAPI::JSONAPI.new(jsonapi_config)
+#  rescue Faraday::Error::ConnectionFailed
+#    JsonApi = nil
+#  end
+#  puts "JSONAPI: Connected" if JsonApi
+#end
+# JSONAPI have been updated, all is fucked now.
+JsonApi = nil
